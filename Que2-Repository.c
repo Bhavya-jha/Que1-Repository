756697dad46b8b475cac35ed9c41bea978a9019d
Bhawani jha <37344784+Bhavya-jha@users.noreply.github.com>
2018-04-15 00:40:47 +0530

    Update Que2-Repository.c

--- Que2-Repository.c
+++ Que2-Repository.c
@@ -11,5 +11,20 @@ void main()
              {
                           scanf("%d",&q[x]);
              }
+	printf("enter the initial position\t");
+	scanf("%d",&head);
+	q[0]=head;
+	printf("\n");
+	for(y=0;y<=size;y++)
+	{
+		difference=abs(q[y+1]-q[y]);
+		seek+=difference;
+		printf("move from %d to %d with seek %d \n",q[y],q[y+1],difference);
+	}
+	printf("\n total distance the disk arm moves %d\t", seek);
+	average=seek/(float)size;
+	printf("\n average seek time is %f\t",average);
+	getch();
+	
             
 			 }
