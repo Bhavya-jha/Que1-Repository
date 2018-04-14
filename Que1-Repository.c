1dbf03f3c297331b5c161b6429a7ac895da7364f
Bhawani jha <37344784+Bhavya-jha@users.noreply.github.com>
2018-04-14 20:38:33 +0530

    Add files via upload

---
+++ Que1-Repository.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+             int q[100],size,head,x,y,seek=0,difference;
+             float average;
+             printf("Size of Queue\t");
+             scanf("%d",&size);
+             printf("Enter the Queue\t");
+             for(x=1;x<=size;x++)
+             {
+                          scanf("%d",&q[x]);
+             }
+         }
