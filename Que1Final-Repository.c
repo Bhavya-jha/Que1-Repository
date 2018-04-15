50f5b7ce8cb71e0596d6b679e459d0648e8326df
Bhawani jha <37344784+Bhavya-jha@users.noreply.github.com>
2018-04-15 19:27:48 +0530

    Add files via upload

---
+++ Que1Final-Repository.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+             int q[100],size,head,x,y,seek=0,difference;
+             float average;
+             printf("Size of Queue\t");
+             scanf("%d",&size);
+             printf("Enter the Queue\t");
+             for(x=1;x<=size;x++)
+             {
+                          scanf("%d",&q[x]);
+             }
+             printf("Enter the initial position\t");
+             scanf("%d",&head);
+             q[0]=head;
+             printf("\n");
+             for(y=0;y<=size-1;y++)
+             {
+                          difference=abs(q[y+1]-q[y]);
+                          seek+=difference;
+                          printf("Move from %d to %d with Seek %d\n",q[y],q[y+1],difference);
+             }
+             printf("\nTotal distance the disk arm moves %d\t",seek);
+             average=seek/(float)size;
+             printf("\nAverage Seek Time is %f\t",average);
+             getch();
+}
+
+
