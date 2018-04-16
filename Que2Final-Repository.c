2ea4de39d0c119f453fa59e9027045a7d622fc15
Bhawani jha <37344784+Bhavya-jha@users.noreply.github.com>
2018-04-16 20:31:22 +0530

    Add files via upload

--- Que2Final-Repository.c
+++ Que2Final-Repository.c
@@ -1,29 +1,68 @@
 #include<stdio.h>
 #include<conio.h>
-main()
+ 
+int main()
 {
-             int q[100],size,head,x,y,seek=0,difference;
-             float average;
-             printf("Size of Queue\t");
-             scanf("%d",&size);
-             printf("Enter the Queue\t");
-             for(x=1;x<=size;x++)
-             {
-                          scanf("%d",&q[x]);
-             }
-             printf("Enter the initial position\t");
-             scanf("%d",&head);
-             q[0]=head;
-             printf("\n");
-             for(y=0;y<=size-1;y++)
-             {
-                          difference=abs(q[y+1]-q[y]);
-                          seek+=difference;
-                          printf("Move from %d to %d with Seek %d\n",q[y],q[y+1],difference);
-             }
-             printf("\nTotal distance the disk arm moves %d\t",seek);
-             average=seek/(float)size;
-             printf("\nAverage Seek Time is %f\t",average);
+    int bursttime[20],p[20],weight[20],tat[20],pr[20],x,j,n,total=0,pos,temp,avg_wt,avg_tat;
+    printf(" Total Number of Process:");
+    scanf("%d",&n);
+ 
+    printf("\n Burst Time and Priority\n");
+    for(x=0;x<n;x++)
+    {
+        printf("\nP[%d]\n",x+1);
+        printf("Burst Time:");
+        scanf("%d",&bursttime[x]);
+        printf("Priority:");
+        scanf("%d",&pr[x]);
+        p[x]=x+1;   
+    }
+ 
+      for(x=0;x<n;x++)
+    {
+        pos=x;
+        for(j=x+1;j<n;j++)
+        {
+            if(pr[j]<pr[pos])
+                pos=j;
+        }
+ 
+        temp=pr[x];
+        pr[x]=pr[pos];
+        pr[pos]=temp;
+ 
+        temp=bursttime[x];
+        bursttime[x]=bursttime[pos];
+        bursttime[pos]=temp;
+ 
+        temp=p[x];
+        p[x]=p[pos];
+        p[pos]=temp;
+    }
+    weight[0]=0;
+   for(x=0;x<n;x++)
+    {
+        weight[x]=0;
+        for(j=0;j<x;j++)
+            weight[x]+=bursttime[j];
+ 
+        total+=weight[x];
+    }
+ 
+    avg_wt=total/n;     
+    total=0;
+ 
+    printf("\nProcess\t    Burst Time    \tWaiting Time\tTurnaround Time");
+    for(x=0;x<n;x++)
+    {
+        tat[x]=bursttime[x]+weight[x];
+        total+=tat[x];
+        printf("\nP[%d]\t\t  %d\t\t    %d\t\t\t%d",p[x],bursttime[x],weight[x],tat[x]);
+    }
+ 
+    avg_tat=total/n;    
+    printf("\n\nAverage Waiting Time is",avg_wt);
+    printf("\nAverage Turnaround Time=%d\n",avg_tat);
+ 
+    return 0;
 }
-
-
